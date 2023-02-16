dd85eba049562fe8f9932178e4e45a99ec44e18c
Tan Nguyen Van <108793627+tannv3201@users.noreply.github.com>
2023-02-16 20:01:22 +0700

    Remove CSS

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const { engine } = require("express-handlebars");
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// Http logger
+app.use(morgan("combined"));
+
+// Template engine
+app.engine(
+    "hbs",
+    engine({
+        extname: ".hbs",
+    })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources\\views"));
+
+app.get("/", (req, res) => {
+    return res.render("home");
+});
+
+app.get("/news", (req, res) => {
+    return res.render("news");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
